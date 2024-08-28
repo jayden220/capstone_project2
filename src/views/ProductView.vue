b3af9c9730ec63ececebce177dcad393e00b6f88
jayden220 <jaydenvanrooyen87@gmail.com>
2024-08-28 09:28:53 +0200

    added contact view

---
+++ src/views/ProductView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iste repudiandae facere harum ullam quos doloremque eius molestias corporis, reprehenderit veniam impedit neque laudantium veritatis delectus magnam error laborum nihil.</h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
